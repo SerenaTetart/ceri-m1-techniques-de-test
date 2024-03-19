958999125da59d2b10c88fa2969b6981066049fb
Serena <Serena.tetart@gmail.com>
2024-03-19 17:43:01 +0100

    TP4

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -16,11 +16,21 @@ public class IPokemonTrainerFactoryTest {
         Pokedex pokedex_1 = (Pokedex) pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory());
         Pokedex pokedex_2 = (Pokedex) pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory());
         PokemonTrainer sacha = new PokemonTrainer("Sacha", Team.VALOR, pokedex_1);
+        assertEquals(sacha.getName(), "Sacha");
+        assertEquals(sacha.getPokedex(), pokedex_1);
+        assertEquals(sacha.getTeam(), Team.VALOR);
         PokemonTrainer laura = new PokemonTrainer("Laura", Team.MYSTIC, pokedex_2);
+        assertEquals(laura.getName(), "Laura");
+        assertEquals(laura.getTeam(), Team.MYSTIC);
 
         PokemonTrainer testTrainer = pokemonTrainerFactory.createTrainer("Sacha", Team.VALOR, pokedexFactory);
-        assertEquals(testTrainer, sacha);
+        assertEquals(testTrainer.getName(), sacha.getName());
+        assertEquals(testTrainer.getPokedex(), sacha.getPokedex());
+        assertEquals(testTrainer.getTeam(), sacha.getTeam());
+
         testTrainer = pokemonTrainerFactory.createTrainer("Laura", Team.MYSTIC, pokedexFactory);
-        assertEquals(testTrainer, laura);
+        assertEquals(testTrainer.getName(), laura.getName());
+        assertEquals(testTrainer.getPokedex(), laura.getPokedex());
+        assertEquals(testTrainer.getTeam(), laura.getTeam());
     }
 }
