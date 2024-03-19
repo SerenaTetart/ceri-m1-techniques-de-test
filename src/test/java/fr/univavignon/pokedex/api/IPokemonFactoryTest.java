8624c973cede5e160627e49d99e160baf98266cd
Serena <Serena.tetart@gmail.com>
2024-03-19 16:58:22 +0100

    TP4

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -13,7 +13,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.Random;
 public class IPokemonFactoryTest {
     @Mock
-    PokemonFactory factory;
+    Pokedex pokedex;
 
     @BeforeEach
     public void setup() {
@@ -23,21 +23,21 @@ public class IPokemonFactoryTest {
     @Test
     public void someTest() throws PokedexException {
         Random rand = new Random();
-        Mockito.when(factory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(
+        Mockito.when(pokedex.createPokemon(0, 613, 64, 4000, 4)).thenReturn(
                 new Pokemon(0, "Bulbizarre", 126+rand.nextInt(16), 126+rand.nextInt(16), 90+rand.nextInt(16), 613, 64, 4000, 4, 56)
         );
-        Mockito.when(factory.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(
+        Mockito.when(pokedex.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(
                 new Pokemon(133, "Aquali", 186+rand.nextInt(16), 168+rand.nextInt(16), 260+rand.nextInt(16), 2729, 202, 5000, 4, 100)
         );
 
-        PokemonMetadata Bulbizarre = factory.createPokemon(0, 613, 64, 4000, 4);
+        PokemonMetadata Bulbizarre = pokedex.createPokemon(0, 613, 64, 4000, 4);
         assertEquals(Bulbizarre.getName(), "Bulbizarre");
         assertEquals(Bulbizarre.getIndex(), 0);
         assertTrue(Bulbizarre.getAttack() >= 126 && Bulbizarre.getAttack() <= 126+15);
         assertTrue(Bulbizarre.getDefense() >= 126 && Bulbizarre.getDefense() <= 126+15);
         assertTrue(Bulbizarre.getStamina() >= 90 && Bulbizarre.getStamina() <= 90+15);
 
-        PokemonMetadata Aquali = factory.createPokemon(133, 2729, 202, 5000, 4);
+        PokemonMetadata Aquali = pokedex.createPokemon(133, 2729, 202, 5000, 4);
         assertEquals(Aquali.getName(), "Aquali");
         assertEquals(Aquali.getIndex(), 133);
         assertTrue(Aquali.getAttack() >= 186 && Aquali.getAttack() <= 186+15);
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -11,28 +11,28 @@ import org.mockito.MockitoAnnotations;
 
 public class IPokemonMetadataProviderTest {
 	@Mock
-    PokemonMetadataProvider provider;
+    Pokedex pokedex;
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
     }
     @Test
     public void someTest() throws PokedexException {
-        Mockito.when(provider.getPokemonMetadata(0)).thenReturn(
+        Mockito.when(pokedex.getPokemonMetadata(0)).thenReturn(
                 new PokemonMetadata(0, "Bulbizarre", 126, 126, 90)
         );
-        Mockito.when(provider.getPokemonMetadata(133)).thenReturn(
+        Mockito.when(pokedex.getPokemonMetadata(133)).thenReturn(
                 new PokemonMetadata(133, "Aquali", 186, 168, 260)
         );
 
-        PokemonMetadata Bulbizarre = provider.getPokemonMetadata(0);
+        PokemonMetadata Bulbizarre = pokedex.getPokemonMetadata(0);
         assertEquals(Bulbizarre.getName(), "Bulbizarre");
         assertEquals(Bulbizarre.getIndex(), 0);
         assertEquals(Bulbizarre.getAttack(), 126);
         assertEquals(Bulbizarre.getDefense(), 126);
         assertEquals(Bulbizarre.getStamina(), 90);
 
-        PokemonMetadata Aquali = provider.getPokemonMetadata(133);
+        PokemonMetadata Aquali = pokedex.getPokemonMetadata(133);
         assertEquals(Aquali.getName(), "Aquali");
         assertEquals(Aquali.getIndex(), 133);
         assertEquals(Aquali.getAttack(), 186);
